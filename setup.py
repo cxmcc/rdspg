cc53893595480bee166b62f9e6b84151715d8559
Xiuming Chen <cc@cxm.cc>
2018-03-05 12:47:50 -0800

    Bump.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ description = ('Command-line toolkit to help understand information '
 
 setup(
     name='rdspg',
-    version='0.1.5',
+    version='0.1.6',
     description=description,
     long_description=long_description,
     author='Xiuming Chen',
